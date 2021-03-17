3baa0387b3ab906f48231900956250c45d72b8f3
Nathan Tellers <nathantellers@gmail.com>
2021-03-16 20:51:51 -0700

    feat: finishes mvp

--- js/script.js
+++ js/script.js
@@ -1,21 +1,42 @@
-alert('js is loaded!');
 // constant variables - data that never changes
+const BASE_URL = 'https://api.spacexdata.com/v3/launches';
+const API_KEY = 'adf;lkakdfl;kjad'; // if you had an API key
 
 // state variables - data that changes
+let launches; // added to global scope to access somewhere else. 
 
 // cached element references - parts of the dom we need to touch
+const $launches = $('#launches');
 
 // event listeners - code designed to capture and respond to events (i.e user clicks on something)
 
+
 // functions - code that represents actions taken/carried out
 
+init()
+
+function init() { // this will run whenever the page first loads in the browser
+    getData();
+}
+
 function getData() {
-    $.ajax('https://api.spacexdata.com/v3/launches')
+    $.ajax(BASE_URL + '?limit=12') // '?' = query parameter
         .then(function (data) {
-            console.log(data);
+            launches = data;
+            render();
         }, function (error) {
             console.log(error);
         });
 }
 
-// console.log(getData())
+function render () {
+    const html = launches.map(function(launch) {
+        return `
+            <article class="card">
+                <h2>${launch.mission_name}</h2>
+                <p>${launch.launch_year}</p>
+            </article>
+        `;
+    });
+    $launches.append(html);
+}
